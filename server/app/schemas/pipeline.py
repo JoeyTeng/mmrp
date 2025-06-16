4488229505d78f8c9b45b2ee1d344960afdad667
Hongyu Teng <hoteng@cisco.com>
2025-06-16 15:25:36 +0100

    style(server): apply formatting rules

--- server/app/schemas/pipeline.py
+++ server/app/schemas/pipeline.py
@@ -1,14 +1,17 @@
 from pydantic import BaseModel
 
+
 class PipelineParameter(BaseModel):
     key: str
     value: str
 
+
 class PipelineModule(BaseModel):
     id: int
     name: str
-    source: list[int]   # list of preceding module(s)
+    source: list[int]  # list of preceding module(s)
     parameters: list[PipelineParameter]
+
 
 class PipelineRequest(BaseModel):
     video: str
