c853a52dd9918d287b922460ab56dc66efc6920d
Everly Precia Suresh <everlyprecia@gmail.com>
2025-06-26 10:32:35 +0000

    review comments

--- client/src/services/pipelineService.ts
+++ client/src/services/pipelineService.ts
@@ -1,9 +1,9 @@
 import type { PipelineRequest } from "@/types/pipeline";
-import { RequestHandler } from "./requestHandler";
+import axios from "axios";
 
 export async function sendPipelineToBackend(
   pipeline: PipelineRequest,
 ): Promise<string[]> {
-  const response = await RequestHandler.post<string[]>("/pipeline", pipeline);
-  return response;
+  const response = await axios.post<string[]>("/pipeline", pipeline);
+  return response.data;
 }
