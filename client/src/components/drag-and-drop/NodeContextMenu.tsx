9c9c8ebd54abd1d685fc5c1a948340776ace7fa7
Brandon Alexander <95941588+brandalexa@users.noreply.github.com>
2025-07-04 16:34:08 +0100

    Create context menu configs for canvas and node

--- client/src/components/drag-and-drop/NodeContextMenu.tsx
+++ client/src/components/drag-and-drop/NodeContextMenu.tsx
@@ -1,94 +1,60 @@
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import { ListItemIcon, ListItemText, Divider } from "@mui/material";
 import {
-  EditOutlined as EditIcon,
-  FileCopyOutlined as DuplicateIcon,
-  DeleteOutlined as DeleteIcon,
-  SettingsOutlined as SettingsIcon,
-  OpenInFullOutlined as OpenInFullIcon,
-  PaletteOutlined as PaletteIcon,
-  FileDownloadOutlined as FileDownloadIcon,
+  OpenInFullOutlined,
+  FileCopyOutlined,
+  DeleteOutlined,
+  SettingsOutlined,
+  EditOutlined,
+  PaletteOutlined,
+  FileDownloadOutlined,
 } from "@mui/icons-material";
+import type { ContextMenuItem } from "./types";
 
-import { NodeAction } from "./types";
+export type NodeAction =
+  | "expand"
+  | "duplicate"
+  | "rename"
+  | "color"
+  | "configure"
+  | "export"
+  | "delete";
 
-interface NodeContextMenuProps {
-  position: {
-    x: number;
-    y: number;
-  } | null;
-  open: boolean;
-  onAction: (action: NodeAction) => void;
-  onClose: () => void;
-}
-
-const ContextMenu = ({
-  position,
-  open,
-  onAction,
-  onClose,
-}: NodeContextMenuProps) => {
-  return (
-    <Menu
-      open={open}
-      onClose={onClose}
-      anchorReference="anchorPosition"
-      anchorPosition={
-        position ? { top: position.y - 6, left: position.x + 2 } : undefined
-      }
-      slotProps={{
-        list: {
-          onContextMenu: (e: React.MouseEvent) => e.preventDefault(),
-        },
-      }}
-    >
-      <MenuItem dense onClick={() => onAction(NodeAction.Expand)}>
-        <ListItemIcon>
-          <OpenInFullIcon />
-        </ListItemIcon>
-        <ListItemText>Expand</ListItemText>
-      </MenuItem>
-      <MenuItem dense onClick={() => onAction(NodeAction.Duplicate)}>
-        <ListItemIcon>
-          <DuplicateIcon />
-        </ListItemIcon>
-        <ListItemText>Duplicate</ListItemText>
-      </MenuItem>
-      <Divider />
-      <MenuItem dense onClick={() => onAction(NodeAction.Rename)}>
-        <ListItemIcon>
-          <EditIcon />
-        </ListItemIcon>
-        <ListItemText>Rename</ListItemText>
-      </MenuItem>
-      <MenuItem dense onClick={() => onAction(NodeAction.Configure)}>
-        <ListItemIcon>
-          <SettingsIcon />
-        </ListItemIcon>
-        <ListItemText>Configure</ListItemText>
-      </MenuItem>
-      <MenuItem dense>
-        <ListItemIcon>
-          <PaletteIcon />
-        </ListItemIcon>
-        <ListItemText>Colour</ListItemText>
-      </MenuItem>
-      <Divider />
-      <MenuItem dense onClick={() => onAction(NodeAction.Export)}>
-        <ListItemIcon>
-          <FileDownloadIcon />
-        </ListItemIcon>
-        <ListItemText>Export</ListItemText>
-      </MenuItem>
-      <MenuItem dense onClick={() => onAction(NodeAction.Delete)}>
-        <ListItemIcon>
-          <DeleteIcon />
-        </ListItemIcon>
-        <ListItemText>Delete</ListItemText>
-      </MenuItem>
-    </Menu>
-  );
-};
-
-export default ContextMenu;
+export const NODE_CONTEXT_MENU: ContextMenuItem<NodeAction>[] = [
+  {
+    id: "expand",
+    label: "Expand",
+    icon: <OpenInFullOutlined />,
+  },
+  {
+    id: "duplicate",
+    label: "Duplicate",
+    icon: <FileCopyOutlined />,
+    dividerAfter: true,
+  },
+  {
+    id: "rename",
+    label: "Rename",
+    icon: <EditOutlined />,
+  },
+  {
+    id: "configure",
+    label: "Configure",
+    icon: <SettingsOutlined />,
+  },
+  {
+    id: "color",
+    label: "Colour",
+    icon: <PaletteOutlined />,
+    dividerAfter: true,
+  },
+  {
+    id: "export",
+    label: "Export",
+    icon: <FileDownloadOutlined />,
+  },
+  {
+    id: "delete",
+    label: "Delete",
+    icon: <DeleteOutlined className="fill-red-700" />,
+    danger: true,
+  },
+];
