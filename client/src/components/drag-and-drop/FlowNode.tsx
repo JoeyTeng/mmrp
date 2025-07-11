87e2041d540b49cc3e08306db19bcc563db8dab5
Brandon Alexander <95941588+brandalexa@users.noreply.github.com>
2025-07-11 15:05:44 +0100

    Remove unused div

--- client/src/components/drag-and-drop/FlowNode.tsx
+++ client/src/components/drag-and-drop/FlowNode.tsx
@@ -21,49 +21,47 @@ export default function FlowNode({
   const MAX_VISIBLE = 4; //default no of params visible in node
 
   return (
-    <div>
-      <div
-        className={`w-40 bg-white rounded-lg overflow-hidden text-sm border ${selected ? "border-black-100" : "border-gray-300"}`}
-      >
-        <div className="pl-3 pr-1 py-1 font-semibold text-gray-800 flex justify-between items-center">
-          {label}
-          <IconButton
-            onClick={(e) => {
-              e.preventDefault();
-              onOpenMenu?.(e, id);
-            }}
-            size="small"
-            aria-label="Module options"
-            sx={{ padding: 0 }}
-          >
-            <MenuIcon />
-          </IconButton>
-        </div>
-        <div className="border-t border-gray-300" />
-        <div className="px-3 py-1 space-y-1">
-          {/* by default show the first 4 params */}
-          {Object.entries(params)
-            .slice(0, MAX_VISIBLE)
-            .map(([key, value]) => (
-              <div key={key} className="flex justify-between">
-                <span className="font-medium text-gray-500">{key}</span>
-                <span className="text-gray-600 max-w-[55%] truncate break-words">
-                  {String(value)}
-                </span>
-              </div>
-            ))}
-          {Object.entries(params).length > MAX_VISIBLE && (
-            <div className="text-left text-gray-400">…</div>
-          )}
-        </div>
-        {type !== NodeType.InputNode ? (
-          <Handle type="target" position={Position.Left} />
-        ) : null}
-
-        {type !== NodeType.OutputNode ? (
-          <Handle type="source" position={Position.Right} />
-        ) : null}
+    <div
+      className={`w-40 bg-white rounded-lg overflow-hidden text-sm border ${selected ? "border-black-100" : "border-gray-300"}`}
+    >
+      <div className="pl-3 pr-1 py-1 font-semibold text-gray-800 flex justify-between items-center">
+        {label}
+        <IconButton
+          onClick={(e) => {
+            e.preventDefault();
+            onOpenMenu?.(e, id);
+          }}
+          size="small"
+          aria-label="Module options"
+          sx={{ padding: 0 }}
+        >
+          <MenuIcon />
+        </IconButton>
       </div>
+      <div className="border-t border-gray-300" />
+      <div className="px-3 py-1 space-y-1">
+        {/* by default show the first 4 params */}
+        {Object.entries(params)
+          .slice(0, MAX_VISIBLE)
+          .map(([key, value]) => (
+            <div key={key} className="flex justify-between">
+              <span className="font-medium text-gray-500">{key}</span>
+              <span className="text-gray-600 max-w-[55%] truncate break-words">
+                {String(value)}
+              </span>
+            </div>
+          ))}
+        {Object.entries(params).length > MAX_VISIBLE && (
+          <div className="text-left text-gray-400">…</div>
+        )}
+      </div>
+      {type !== NodeType.InputNode ? (
+        <Handle type="target" position={Position.Left} />
+      ) : null}
+
+      {type !== NodeType.OutputNode ? (
+        <Handle type="source" position={Position.Right} />
+      ) : null}
     </div>
   );
 }
