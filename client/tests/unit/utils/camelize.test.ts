8783d577a6303c1c5db7c5aace60dbf63a045f61
Everly Precia Suresh <everlyprecia@gmail.com>
2025-07-24 14:53:28 +0000

    fix: review comments

--- client/tests/unit/utils/camelize.test.ts
+++ client/tests/unit/utils/camelize.test.ts
@@ -1,6 +1,49 @@
 import { camelizeKeys } from "@/utils/camelize";
-import { snakeCasedModules } from "../fixtures/moduleResponse";
 import type { ModuleMeta } from "@/types/module";
+
+export const snakeCasedModules = [
+  {
+    id: 0,
+    name: "blur",
+    role: "process_node",
+    parameters: [
+      {
+        name: "kernel_size",
+        type: "int",
+        description: null,
+        default: 5,
+        constraints: null,
+        required: true,
+      },
+      {
+        name: "method",
+        type: "str",
+        description: null,
+        default: "gaussian",
+        constraints: ["gaussian", "median", "bilateral"],
+        required: true,
+      },
+    ],
+    input_formats: [
+      {
+        pixel_format: "bgr24",
+        color_space: "BT.709 Full",
+        width: null,
+        height: null,
+        frame_rate: null,
+      },
+    ],
+    output_formats: [
+      {
+        pixel_format: "bgr24",
+        color_space: "BT.709 Full",
+        width: null,
+        height: null,
+        frame_rate: null,
+      },
+    ],
+  },
+];
 
 describe("camelizeKeys()", () => {
   /** Recursively asserts that no object key contains an underscore */
