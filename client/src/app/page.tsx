8fe7a0b5a2663b808b0c1e9eaa2c7fb1c282797e
Brandon Alexander <95941588+brandalexa@users.noreply.github.com>
2025-07-30 11:46:47 +0100

    Clean up providers

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,31 +1,27 @@
 import DragAndDropArea from "@/components/cards/DragAndDropArea";
 import AppLayout from "@/components/layout/AppLayout";
-import { VideoReloadProvider } from "@/contexts/videoReloadContext";
 import { Box } from "@mui/material";
 import { ToastContainer } from "react-toastify/unstyled";
 import "react-toastify/ReactToastify.css";
-import { VideoMetricsProvider } from "@/contexts/VideoMetricsContext";
 import VideoComparisonView from "@/components/cards/VideoComparisonView";
-import { WebSocketProvider } from "@/contexts/WebSocketContext";
+import AppProviders from "@/contexts/AppProviders";
 
 export default function Home() {
   return (
-    <VideoMetricsProvider>
-      <VideoReloadProvider>
-        <AppLayout>
-          <Box className="flex flex-col h-full gap-2">
-            {/* Video Comparison View - Fixed height with min-height */}
-            <Box className="h-1/3 min-h-[350px]">
-              <VideoComparisonView />
-            </Box>
-            {/* Flow Canvas - Takes remaining space with min-height */}
-            <Box className="h-2/3 flex-1">
-              <DragAndDropArea />
-            </Box>
+    <AppProviders>
+      <AppLayout>
+        <Box className="flex flex-col h-full gap-2">
+          {/* Video Comparison View - Fixed height with min-height */}
+          <Box className="h-1/3 min-h-[350px]">
+            <VideoComparisonView />
           </Box>
-          <ToastContainer />
-        </AppLayout>
-      </VideoReloadProvider>
-    </VideoMetricsProvider>
+          {/* Flow Canvas - Takes remaining space with min-height */}
+          <Box className="h-2/3 flex-1">
+            <DragAndDropArea />
+          </Box>
+        </Box>
+        <ToastContainer />
+      </AppLayout>
+    </AppProviders>
   );
 }
