4d53435e302b1112f2412b7a527b5388393250bf
Brandon Alexander <95941588+brandalexa@users.noreply.github.com>
2025-08-07 10:49:08 +0100

    Add special header for input and output nodes

--- client/src/components/drag-and-drop/Modules.tsx
+++ client/src/components/drag-and-drop/Modules.tsx
@@ -1,45 +1,50 @@
 import { Box, Divider, InputAdornment, Stack, TextField } from "@mui/material";
-import { useContext, useMemo, useState } from "react";
-import { ModulesContext } from "@/contexts/ModulesContext";
+import { useMemo, useState } from "react";
+import { useModulesContext } from "@/contexts/ModulesContext";
 import { SearchOutlined } from "@mui/icons-material";
 import Fuse from "fuse.js";
 import ModuleItemGroup from "./ModuleItemGroup";
+import { NodeType } from "@/types/module";
 
 export default function Modules() {
   const [searchQuery, setSearchQuery] = useState("");
+  const searchQueryTrimmed = searchQuery.trim();
 
-  const modules = useContext(ModulesContext).sort((a, b) =>
-    a.name.localeCompare(b.name),
-  );
+  const modules = useModulesContext();
+  const modulesSorted = useMemo(() => {
+    return [...modules.values()].sort((a, b) => a.name.localeCompare(b.name));
+  }, [modules]);
 
   type GroupType = Record<
     string,
     { modules: typeof modules; minScore: number }
   >;
 
-  const fuse = useMemo(
-    () =>
-      new Fuse(modules, {
-        keys: [
-          { name: "name", weight: 0.6 },
-          { name: "type", weight: 0.3 },
-          { name: "data.parameters.name", weight: 0.1 },
-        ],
-        includeScore: true,
-        threshold: 0.3,
-      }),
-    [modules],
-  );
+  const fuse = useMemo(() => {
+    return new Fuse(modulesSorted, {
+      keys: [
+        { name: "name", weight: 0.6 },
+        { name: "type", weight: 0.3 },
+        { name: "data.parameters.name", weight: 0.1 },
+      ],
+      includeScore: true,
+      threshold: 0.3,
+    });
+  }, [modulesSorted]);
 
   const filteredModules = useMemo(() => {
-    if (!searchQuery)
-      return modules.map((module) => ({ item: module, score: 1 }));
-    return fuse.search(searchQuery);
-  }, [fuse, modules, searchQuery]);
+    if (!searchQueryTrimmed)
+      return modulesSorted.map((module) => ({ item: module, score: 1 }));
+    return fuse.search(searchQueryTrimmed);
+  }, [fuse, modulesSorted, searchQueryTrimmed]);
 
   const groupedModules = useMemo(() => {
     const grouped = filteredModules.reduce((accumulator, currentValue) => {
-      const key = currentValue.item.name.charAt(0).toUpperCase();
+      const key =
+        currentValue.item.type === NodeType.InputNode ||
+        currentValue.item.type === NodeType.OutputNode
+          ? "Input and Output"
+          : currentValue.item.name.charAt(0).toUpperCase();
       if (!accumulator[key]) {
         accumulator[key] = {
           modules: [currentValue.item],
@@ -55,16 +60,13 @@ export default function Modules() {
       return accumulator;
     }, {} as GroupType);
 
-    console.log(grouped);
     return grouped;
   }, [filteredModules]);
-  console.log(modules);
 
   if (!modules) return;
   return (
-    <Box>
-      {/* <Box className="flex overflow-hidden h-full w-full flex-col"> */}
-      <Box mb={2} mt={1}>
+    <Box className="flex flex-col h-full">
+      <Box mb={2} mt={1} className="shrink-0">
         <TextField
           autoFocus
           fullWidth
@@ -85,19 +87,15 @@ export default function Modules() {
         />
       </Box>
       <Divider variant="fullWidth" className="mb-4" aria-hidden="true" />
-      {/* <Box className="flex flex-wrap gap-2"> */}
-      <Stack spacing={1} width={"100%"}>
-        {/* {filteredModules.map((item) => (
-          <ModuleItem
-            key={item.id}
-            id={item.id}
-            name={item.name}
-            moduleClass={item.moduleClass}
-            parameters={item.data.parameters}
-          />
-        ))} */}
+      <Stack spacing={1} width={"100%"} className="overflow-y-auto flex-1">
         {Object.entries(groupedModules)
-          .sort((a, b) => a[1].minScore - b[1].minScore)
+          .sort(([aKey, aVal], [bKey, bVal]) => {
+            if (!searchQueryTrimmed) {
+              if (aKey === "Input and Output") return -1;
+              if (bKey === "Input and Output") return 1;
+            }
+            return aVal.minScore - bVal.minScore;
+          })
           .map(([title, moduleGroup]) => (
             <ModuleItemGroup
               key={title}
