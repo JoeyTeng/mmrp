d10f3efa6a51102459c6cbac250c7feb417d0389
Frank Cruz <francruz_08@hotmail.com>
2025-08-12 17:42:51 +0100

    Remove extra comma

--- client/src/types/module.ts
+++ client/src/types/module.ts
@@ -57,7 +57,7 @@ export interface FormatDefinition {
   colorSpace?: string[];
   width?: number;
   height?: number;
-  frameRate?: FrameRate;
+  frameRate?: FrameRate; //fps
 }
 
 export enum ModuleType {
