JoeyTeng/mmrp

commit 40db3efdfbf37a0de6708fb8623dbe012f5b9af5

Enhancement: Include tests as part of pre-commit hooks (#88)

server/.lintstagedrc.cjs
@@ -3,5 +3,6 @@ module.exports = {
     "uv --project server run pyright -p server",
     "uv --project server run ruff check --fix --show-fixes",
     "uv --project server run ruff format",
+    "uv --project server run pytest"
   ],
 };

client/.lintstagedrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
     "eslint --config client/eslint.config.mjs --cache --fix",
     () => "tsc --project client --noEmit",
     format,
+    () => "npm --prefix client run test:unit",
   ],
   "*.{css,json,md}": format,
 };

commit c54130089554dc662eed1c458cf84f36e709908f

build: fix pre-commit hook `pytest` for server side
Lint-staged will automatically pass-in the changed file as positional argument to the command, which will change the behaviour of `pytest`.
The new change prevents this behaviour and always run the full set of unit-tests.

Fix bug introduced in #88.

server/.lintstagedrc.cjs
@@ -3,6 +3,6 @@ module.exports = {
     "uv --project server run pyright -p server",
     "uv --project server run ruff check --fix --show-fixes",
     "uv --project server run ruff format",
-    "uv --project server run pytest"
+    () => "uv --project server run pytest -c server/pyproject.toml" // to prevent lint-staged to pass in any positional argument to interfere with pytest.
   ],
 };