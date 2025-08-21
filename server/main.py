0954f2957b2b456ba41923d3a6ddf594c6e8aeeb
Romy Richter <romy_richter01@yahoo.de>
2025-08-21 20:57:40 +0100

    adjust files for it to work on the vm

--- server/main.py
+++ server/main.py
@@ -1,12 +1,22 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from pathlib import Path
 import shutil
+from fastapi.staticfiles import StaticFiles
 import uvicorn
 from app.routers import pipeline, video, modules, frame, binaries
 from app.db.convert_json_to_modules import get_all_mock_modules
 from app.services.binaries import download_gist_files, sync_binaries
+
+
+api = APIRouter(prefix="/api")
+
+api.include_router(pipeline.router)
+api.include_router(video.router)
+api.include_router(modules.router)
+api.include_router(frame.router)
+api.include_router(binaries.router)
 
 
 @asynccontextmanager
@@ -40,21 +50,31 @@ async def lifespan(app: FastAPI):
         print(f"Error during cleanup: {e}")
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json", lifespan=lifespan)
+app.include_router(api)
+
+origins = ["http://localhost:3000", "http://10.47.53.99"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # frontend
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router(pipeline.router)
-app.include_router(video.router)
-app.include_router(modules.router)
-app.include_router(frame.router)
-app.include_router(binaries.router)
+# Only mount static if the export exists (production)
+out_dir = Path(__file__).resolve().parents[1] / "client" / "out"
+if out_dir.exists():
+    app.mount("/", StaticFiles(directory=str(out_dir), html=True), name="static")
+else:
+
+    @app.get("/")
+    def dev_root():
+        return {
+            "msg": "Dev mode: Run `npm run dev` on port 3000, or build the client to create /client/out."
+        }
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
