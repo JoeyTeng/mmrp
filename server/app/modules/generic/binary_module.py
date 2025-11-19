aa6b0dbb71ecda3b0373efb3557dcbeb631ce569
Tim Swanson <tiswanso@cisco.com>
2025-11-19 18:21:45 -0500

    fix: add copyright and license headers to **/*.py
    Signed-off-by: Tim Swanson <tiswanso@cisco.com>

--- server/app/modules/generic/binary_module.py
+++ server/app/modules/generic/binary_module.py
@@ -1,3 +1,7 @@
+# Copyright 2025 Cisco Systems, Inc. and its affiliates
+#
+# SPDX-License-Identifier: Apache-2.0
+
 import numpy as np
 from app.modules.module import ModuleBase
 from typing import Any, override
--- server/app/modules/module.py
+++ server/app/modules/module.py
@@ -1,3 +1,7 @@
+# Copyright 2025 Cisco Systems, Inc. and its affiliates
+#
+# SPDX-License-Identifier: Apache-2.0
+
 from abc import ABC, abstractmethod
 from typing import Any
 from pydantic import BaseModel, Field, ConfigDict
